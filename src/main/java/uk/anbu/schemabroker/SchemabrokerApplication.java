6c08961ed67c0006b63e8cc0564cddc651315392
anbu <git@anbu.uk>
2026-02-24 17:38:44 +0000

    Project init with just the liquibase yaml files.

---
+++ src/main/java/uk/anbu/schemabroker/SchemabrokerApplication.java
@@ -0,0 +1,13 @@
+package uk.anbu.schemabroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchemabrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchemabrokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/anbu/schemabroker/SchemabrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.anbu.schemabroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemabrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
